152299f115676e49c15d7d689b2ae9bc2a46fe1f
Hallur20 <hvn15@hotmail.com>
2019-03-31 15:42:07 +0200

    print in algs4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import edu.princeton.cs.algs4.StdOut;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,7 +18,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("yoyo");
+        StdOut.println("Hello World");
     }
     
 }
