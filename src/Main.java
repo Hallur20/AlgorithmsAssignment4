c7286cb78bcb0d1c51509d7cd4d7061b9ba77f6c
Mokayed <marik.3000@hotmail.com>
2019-04-07 01:55:26 +0200

    time and dis

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import models.Graph;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,9 +20,14 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        StdOut.println(in.readAll());
+   public static void main(String[] args) {
+      String filePath = "C:\\Users\\MoK\\Desktop\\MakeDijkstraWork\\src\\makedjkstrawork\\routes.txt";
+        Graph graph = new Graph();
+        Dijkstra dj = new Dijkstra();
+        //dj.shotestDistance(graph);
+        dj.shotestTime(graph,filePath);
+        dj.printShortestPaths(graph);
+
     }
     
 }
---
+++ src/models/Graph.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author MoK
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+public class Graph {
+
+    private Set<Node> nodes = new HashSet<>();
+
+    public void addNode(Node nodeA) {
+        nodes.add(nodeA);
+    }
+
+    public Set<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Set<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    @Override
+    public String toString() {
+        return "Graph{" + "nodes=" + nodes + '}';
+    }
+}
